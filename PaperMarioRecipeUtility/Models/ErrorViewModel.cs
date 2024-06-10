f6b9df8b63d345a043ef63e388f8770140f63494
Parsley Winters <winters.parker.s@gmail.com>
2024-06-09 20:23:22 -0400

    Initial commit

---
+++ PaperMarioRecipeUtility/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PaperMarioRecipeUtility.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
